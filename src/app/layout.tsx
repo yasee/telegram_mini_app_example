b47db5a8d48b01a1e4e484aa822a723fb63d25cd
yasee <779523+yasee@users.noreply.github.com>
2024-09-29 11:03:31 +0900

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <script src="https://telegram.org/js/telegram-web-app.js"></script>
+            </head>
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
