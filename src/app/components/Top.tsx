45fb476e090f1ce27344929f2a6cd65e934991f7
yasee <779523+yasee@users.noreply.github.com>
2024-09-29 11:05:21 +0900

    remove debug code

--- src/app/components/Top.tsx
+++ src/app/components/Top.tsx
@@ -28,7 +28,6 @@ export default function Top() {
                     alert(data.message);
                 }
             });
-        setMessage("Hello, world!");
     };
     return (
         <div>
